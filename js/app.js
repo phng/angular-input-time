06a63f7b709c85cd9fc2c531bfc2cbeda08c9295
phuongnguyen <phuong.nguyen@zengroupllc.com>
2015-07-05 13:46:48 -0500

    initial

---
+++ js/app.js
@@ -0,0 +1,23 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('inputTimeDemoApp', ['png.timeinput'])
+        .controller('mainController', function ($scope) {
+            var vm = this;
+
+            vm.isChrome = /chrome/i.test(navigator.userAgent);
+
+            vm.setCurrentTime = function () {
+                vm.time = new Date();
+            };
+
+            vm.clearTime = function () {
+                vm.time = null;
+            };
+
+            $scope.$watch('main.time', function (value) {
+                if (!value) return;
+                vm.time = new Date(value.toISOString());
+            }, true)
+        })
+})(angular);
